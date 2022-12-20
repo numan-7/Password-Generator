ce456fc3fe80b7cb336d5f1e314c6d64fd3540e3
Numan Khan <101899800+numan-7@users.noreply.github.com>
2022-12-19 16:00:42 -0800

    finalizing ui

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const lowerCharacters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n"
 const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 const specialCharacters= ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?", "/"];
 
-
+const passBtn = document.getElementById("psw-btn")
 
 
