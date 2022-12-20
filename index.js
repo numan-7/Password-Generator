f236aa4dc805024de0c7d3e23f95bdf86ae669f1
Numan Khan <101899800+numan-7@users.noreply.github.com>
2022-12-19 17:49:51 -0800

    css & html

--- index.js
+++ index.js
@@ -3,6 +3,79 @@ const lowerCharacters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n"
 const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 const specialCharacters= ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?", "/"];
 
-const passBtn = document.getElementById("psw-btn")
+let passBtn = document.getElementById('psw-btn');
+let passRange = document.getElementById("myRange");
+let passLengthCounter = document.getElementById("psw-adjust");
+let copyBtn = document.getElementById("copy-btn");
 
+let passwordLength = 10;
+
+let checkBoxes = {
+    uppercase: false,
+    lowercase: false,
+    nums: false,
+    specChars: false,
+}
+
+// check boxes
+document.getElementById("ABC").addEventListener("click", (event) => {
+    if(event.target.checked){
+        checkBoxes[0] = true;
+    } else {
+        checkBoxes[0] = false;
+    } 
+});
+document.getElementById("abc").addEventListener("click", (event) => {
+    if(event.target.checked){
+        checkBoxes[1] = true;
+    } else {
+        checkBoxes[1] = false;
+    }
+});
+document.getElementById("123").addEventListener("click", (event) => {
+    if(event.target.checked){
+        checkBoxes[2] = true;
+    } else {
+        checkBoxes[2] = false;
+    }
+});
+document.getElementById("#$&").addEventListener("click", (event) => {
+    if(event.target.checked){
+        checkBoxes[2] = true;
+    } else {
+        checkBoxes[2] = false;
+    }
+});
+
+passBtn.addEventListener("click", event => {
+    
+});
+
+// update the pass length var when a person clicks the slider
+passRange.addEventListener("click", event => {
+passwordLength = event.target.value;
+  passLengthCounter.textContent = "adjust password length: " + passwordLength;
+});
+// update the pass length var when a person uses arrows to adjust the slider
+passRange.addEventListener("keyup", event => {
+    passwordLength = event.target.value;
+      passLengthCounter.textContent = "adjust password length: " + passwordLength;
+});
+copyBtn.addEventListener("click", event => {
+    let copyText = document.getElementById("pass-btn");
+    // Select the text field
+    content = copyText.textContent;
+    // Copy the text inside the text field
+    navigator.clipboard.writeText(content);
+    alert("Copied the text: " + content);
+});
+// checks if one of the checkboxes have been checked
+function checkCheck(){
+    if(!uppercase && !lowercase && !nums && !specChars){
+        alert("Please Check One of The Checkboxes to Generate");
+        return false;
+    } else {
+        return true;
+    }
+}
 
